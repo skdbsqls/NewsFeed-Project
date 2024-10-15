81d1646117ca75df4f7d094b44ad06ea9c40f2dc
Yunbeen Na <skdbsqls98@naver.com>
2024-10-16 00:27:32 +0900

    feat: update mainheader, newspage, newsdetailpage

---
+++ app/news/[slug]/page.js
@@ -0,0 +1,16 @@
+import { DUMMY_NEWS } from "@/dummy-news";
+
+export default function NewsDetailPage({ params }) {
+  const newsSlug = params.slug;
+  const news = DUMMY_NEWS.find((news) => news.slug === newsSlug);
+  return (
+    <article className="news-article">
+      <header>
+        <img src={`/images/news/${news.image}`} alt={news.title} />
+        <h1>{news.title}</h1>
+        <time dateTime={news.date}>{news.date}</time>
+      </header>
+      <p>{news.content}</p>
+    </article>
+  );
+}
