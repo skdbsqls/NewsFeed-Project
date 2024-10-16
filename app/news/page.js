795fdff54daa765223343d79d3e7437aa90fc157
Yunbeen Na <skdbsqls98@naver.com>
2024-10-16 17:06:21 +0900

    feat: add archivepage and separate newslist component

--- app/news/page.js
+++ app/news/page.js
@@ -1,21 +1,11 @@
-import Link from "next/link";
-
 import { DUMMY_NEWS } from "@/dummy-news";
+import NewsList from "@/components/news-list";
 
 export default function NewsPage() {
   return (
     <>
       <h1>News Page</h1>
-      <ul className="news-list">
-        {DUMMY_NEWS.map((news) => (
-          <li key={news.id}>
-            <Link href={`/news/${news.slug}`}>
-              <img src={`images/news/${news.image}`} alt={news.title} />
-              <span>{news.title}</span>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <NewsList news={DUMMY_NEWS} />
     </>
   );
 }
