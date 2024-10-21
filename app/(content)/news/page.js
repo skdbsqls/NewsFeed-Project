f6cafb4dbd2a03b61fe20ee10df525775af0736f
Yunbeen Na <skdbsqls98@naver.com>
2024-10-21 13:05:12 +0900

    feat: fetch news data from client side

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,11 +1,51 @@
-import { DUMMY_NEWS } from "@/dummy-news";
+// 클라이언트 측 데이터 가져오기
+"use client";
+
+import { useEffect, useState } from "react";
+
 import NewsList from "@/components/news-list";
 
 export default function NewsPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [news, setNews] = useState();
+
+  useEffect(() => {
+    async function fetchNews() {
+      setIsLoading(true);
+      const response = await fetch("http://localhost:8080/news");
+
+      if (!response.ok) {
+        setError("Failed to fetch news.");
+        setIsLoading(false);
+      }
+
+      const news = await response.json();
+      setIsLoading(false);
+      setNews(news);
+    }
+
+    fetchNews();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  let newsContent;
+
+  if (news) {
+    newsContent = <NewsList news={news} />;
+  }
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {newsContent}
     </>
   );
 }
