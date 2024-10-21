b12801c70849ce325c4c7d07a69c747959b685d6
Yunbeen Na <skdbsqls98@naver.com>
2024-10-21 13:53:32 +0900

    fix: update newsdata using local database

--- app/(content)/archive/@archive/[[...filter]]/page.js
+++ app/(content)/archive/@archive/[[...filter]]/page.js
@@ -7,7 +7,7 @@ import {
 } from "@/lib/news";
 import Link from "next/link";
 
-export default function FilterNewsPage({ params }) {
+export default async function FilterNewsPage({ params }) {
   // archive 이후의 모든 세그먼트들이 배열로 담김
   const filter = params.filter;
 
@@ -17,16 +17,16 @@ export default function FilterNewsPage({ params }) {
 
   // 연도 선택 or 연도 및 월 선택에 맞게 해당 뉴스를 보여줌
   let news; // 보여질 뉴스 배열
-  let links = getAvailableNewsYears(); // 네비바에 보여질 연도 혹은 월의 배열
+  let links = await getAvailableNewsYears(); // 네비바에 보여질 연도 혹은 월의 배열
 
   // 연도만 선택한 경우
   if (selectedYear && !selectedMonth) {
-    news = getNewsForYear(selectedYear);
+    news = await getNewsForYear(selectedYear);
     links = getAvailableNewsMonths(selectedYear); // links를 연도에서 월로 업데이트
   }
   // 연도 및 월 모두 선택한 경우
   if (selectedYear && selectedMonth) {
-    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    news = await getNewsForYearAndMonth(selectedYear, selectedMonth);
     links = []; // links를 월에서 빈 배열로 업데이트
   }
 
@@ -37,12 +37,13 @@ export default function FilterNewsPage({ params }) {
     newsContent = <NewsList news={news} />;
   }
 
+  const availableYears = await getAvailableNewsYears();
+
   // 유효하지 않은 연도 혹은 월에 접근할 경우 오류 발생
   if (
-    (selectedYear &&
-      !getAvailableNewsYears(selectedYear).includes(+selectedYear)) ||
+    (selectedYear && !availableYears.includes(selectedYear)) ||
     (selectedMonth &&
-      !getAvailableNewsMonths(selectedYear).includes(+selectedMonth))
+      !getAvailableNewsMonths(selectedYear).includes(selectedMonth))
   ) {
     throw new Error("Invalid filter.");
   }
